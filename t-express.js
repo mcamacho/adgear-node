920d1609d977471637398649582bfa392ae13a81
Mauricio Camacho <maurocamacho@gmail.com>
2015-02-23 15:38:58 -0500

    some sanity

--- t-express.js
+++ t-express.js
@@ -1,29 +1,34 @@
 /*global require*/
+(function () {
+"use strict";
+
+// require ext modules
 var path = require("path");
 var request = require("request");
 var lodash = require("lodash");
 var express = require("express");
 var ejs = require("ejs");
-var app = express();
 
+// constant properties
 var adgeardomain = "api.admin.adgear.com";
 var campaignlist = "/reports/campaign/agency_campaigns";
 var campaignbase = "/reports/campaigns";
 
+// get campaign path
 function campaignid(ele) {
-  "use strict";
   return "/" + ele.id + "-" + ele.name.toLowerCase().replace(/\s-\s|\s/g, "-") + "/delivery";
 }
 
-// var campaignsschema = { "overall_ctr": "number", "items": "object", "_urls": "object", "total_clicks": "number", "total_impressions": "number" };
-
-var options = {
+// http digest settings
+var reqoptions = {
   "auth": {
     "user": "mauricio",
     "pass": "0cbdfcd7cc2c5b099022adcd78cd7c90da210f46d20b09809d485d0cc1cf21b1",
     "sendImmediately": false
   }
 };
+
+// methods to include on the data sent to the render view
 var ejshelpers = {
   gettype: function (ele) {
     return typeof ele;
@@ -31,17 +36,20 @@ var ejshelpers = {
   campaignid: campaignid
 };
 
-// console.log(ejs);
+
+// instance an express server
+var app = express();
+// configure server
 app.engine("html", ejs.renderFile);
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "html");
-// get all campaigns
+
+// set route for all campaigns
 app.get("/", function (req, res) {
-  "use strict";
-  lodash.assign(options, {
+  lodash.assign(reqoptions, {
     "uri": "http://" + adgeardomain + campaignlist + ".json"
   });
-  request(options, function (e, r, b) {
+  request(reqoptions, function (e, r, b) {
     if (!e && r.statusCode === 200) {
       res.render("home", {
         data: JSON.parse(b),
@@ -50,15 +58,14 @@ app.get("/", function (req, res) {
     }
   });
 });
-// get specific campaign
+// set route for specific campaign
 app.get(/.*delivery$/, function (req, res) {
-  "use strict";
-  lodash.assign(options, {
+  lodash.assign(reqoptions, {
     "uri": "http://" + adgeardomain + campaignbase + req.url + ".json"
   });
-  request(options, function (e, r, b) {
+  request(reqoptions, function (e, r, b) {
     if (!e && r.statusCode === 200) {
-      console.log(JSON.parse(b));
+      // console.log(JSON.parse(b));
       res.render("campaign", {
         data: JSON.parse(b),
         help: ejshelpers
@@ -68,3 +75,4 @@ app.get(/.*delivery$/, function (req, res) {
 });
 
 app.listen(3000);
+}());
